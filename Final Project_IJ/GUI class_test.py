965b62216b0940d4feec7c315ff2d0f786d3237b
Ina Jung <ina.jung84@gmail.com>
2023-12-10 17:37:46 -0500

    Added Classes for each window

--- Final Project_IJ/GUI class_test.py
+++ Final Project_IJ/GUI class_test.py
@@ -2,34 +2,44 @@ from breezypythongui import EasyFrame
 import tkinter as tk
 from breezypythongui import EasyFrame
 from tkinter import PhotoImage, Label, Button, StringVar, DoubleVar, messagebox
-        
+
 class MainFrame(EasyFrame):
     
     def __init__(self):
         EasyFrame.__init__(self, title="Money Clip", height=800, width=880)
-        
+
         money_clip_label = self.addLabel("Money Clip", row=0, column=1,columnspan = 2)
-        
         money_clip_label["font"] = ("Arial", 40)
         money_clip_label["foreground"] = "#385492"
         money_clip_label["background"] = "#7FE7FE"
         money_clip_label.place(x=350, y=10)
-
         self.round_w = 0
         self.round_1 = 0
-        
         self.background_image = PhotoImage(file="image/main_background_re.png")
         background_label = Label(self, image=self.background_image)
         background_label.place(relwidth=1, relheight=1)  # Cover the entire window
-
         money_clip_label = Label(self, text="Money Clip", font=("Arial", 40), fg="#385492", bg="#7FE7FE")
         money_clip_label.place(x=350, y=10)
-        
-        
-    def round_won(self):
-        pass
-    def round_lost(self):
-        pass
+
+        # add CreatePlan button
+        create_plan_button = self.addButton(text="Create Plan", row=1, column=1, command=self.start_create_plan)
+        # add Spend button
+        spend_button = self.addButton(text="Spend", row=1, column=2, command=self.start_spend)
+        # add CheckStatus button
+        check_status_button = self.addButton(text="Check Status", row=1, column=3, command=self.start_check_status)
+
+    def start_create_plan(self):
+        create_plan_window = CreatePlan()
+        create_plan_window.grid(row=0, column=0, sticky ="NSEW")
+
+    def start_spend(self):
+        spend_window = Spend()
+        spend_window.grid(row=0, column=0, sticky ="NSEW")
+
+    def start_check_status(self):
+        check_status_window = CheckStatus()
+        check_status_window.grid(row=0, column=0, sticky ="NSEW")
+
 
 class CreatePlan(EasyFrame):
     def __init__(self):
@@ -39,13 +49,63 @@ class CreatePlan(EasyFrame):
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
         self.round_w = 0
         self.round_1 = 0
+        # add TextBox in certer of the screen
+        self.addLabel(text="Make Your Plan", row=0, column=10, font=("Arial", 30) )
+        # add CreateNewPlan button
+        create_new_plan_button = self.addButton(text="Create New Plan", row=1, column=10, command=self.start_add_new_plan)
+        # add ChangePlan button
+        change_plan_button = self.addButton(text="Change Plan", row=2, column=10, command=self.start_change_plan)
+        # add finish button
+        finish_btn = tk.Button(self, text="Back to main", command=self.finish_create_plan)
+        finish_btn.grid(row=0, column=0)
+
+    def start_add_new_plan(self):
+        create_new_plan_window = CreateNewPlan()
+        create_new_plan_window.grid(row=0, column=0, sticky ="NSEW")
+
+    def start_change_plan(self):
+        change_plan_window = ChangePlan()
+        change_plan_window.grid(row=0, column=0, sticky ="NSEW")
+
+    def finish_create_plan(self):
+        self.grid_forget()
+
+class CreateNewPlan(EasyFrame):
+    def __init__(self):
+        EasyFrame.__init__(self, title="Money Clip", height=800, width=880)
+        self.background_image = PhotoImage(file="image/budget_back.png")
+        background_label = Label(self, image=self.background_image)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.round_w = 0
+        self.round_1 = 0
+        # add finish button
+        finish_btn = tk.Button(self, text="Finish", command=self.finish_add_new_plan)
+        finish_btn.grid(row=0, column=0)
+
+    def finish_add_new_plan(self):
+        self.grid_forget()
+
+class ChangePlan(EasyFrame):
+    def __init__(self):
+        EasyFrame.__init__(self, title="Money Clip", height=800, width=880)
+        self.background_image = PhotoImage(file="image/budget_back.png")
+        background_label = Label(self, image=self.background_image)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.round_w = 0
+        self.round_1 = 0
+        # add finish button
+        finish_btn = tk.Button(self, text="Finish", command=self.finish_change_plan)
+        finish_btn.grid(row=0, column=0)
+
+    def finish_change_plan(self):
+        self.grid_forget()
+
 
 def start_create_plan():
     global create_plan_window
     create_plan_window = CreatePlan()
     create_plan_window.grid(row=0, column=0, sticky ="NSEW")
-    finish_btn = tk.Button(create_plan_window, text="Back to main", command=finish_create_plan)
-    finish_btn.grid(row=0, column=0)
+
     
 def finish_create_plan():
     create_plan_window.grid_forget()
